0b8ab5450989d0cac690230c74cf0172a41a4bbf
Pedro <pedrolima7p@gmail.com>
2025-05-26 23:45:02 -0300

    v1 potifolio

--- js/video.js
+++ js/video.js
@@ -2,7 +2,7 @@ function loadVideos() {
     const videoPaths = [
         'videos/video1.mp4',
         'videos/v8 (1) (1).mp4',
-        'videos/video5.MOV',
+        'videos/V618.mov',
         'videos/video6.mp4',
         'videos/video9.mov',
         'videos/video3.mp4',
